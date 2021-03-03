df45cdd5a2b8af85c5678655c211988a028c4447
Daniel Gonzalez <gon20293@uvg.edu.gt>
2021-03-02 20:48:15 -0600

    Laboratorio 7 Videojuegos UVG

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    NavMeshAgent agent;
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        agent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit rayInfo;
+        Vector3 pos = Input.mousePosition;
+        Ray myRay = Camera.main.ScreenPointToRay(pos);
+        if(Physics.Raycast(myRay, out rayInfo) && Input.GetMouseButton(0))
+        {
+            agent.SetDestination(rayInfo.point);
+        }
+        animator.SetFloat("Velocity", Mathf.Abs(agent.velocity.x));
+
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Villain.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Villain : MonoBehaviour
+{
+    NavMeshAgent agent;
+    public GameObject wayPointsObject;
+    Animator animator;
+    List<Vector3> wayPoints;
+    int currentWayPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
+        wayPoints = new List<Vector3>();
+        if (wayPointsObject)
+        {
+            foreach (Transform child in wayPointsObject.transform)
+            {
+                wayPoints.Add(child.position);
+            }
+        }
+
+        agent.obstacleAvoidanceType = ObstacleAvoidanceType.NoObstacleAvoidance;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        animator.SetFloat("Velocity", Mathf.Abs(agent.velocity.x));
+        int position = Random.Range(0, wayPoints.Count);
+        if (agent)
+        {
+            if (agent.remainingDistance < 0.5f)
+            {
+                int newWP = currentWayPoint;
+                while (newWP == currentWayPoint)
+                    newWP = Random.Range(0, wayPoints.Count);
+
+                currentWayPoint = newWP;
+                agent.SetDestination(wayPoints[currentWayPoint]);
+            }
+        }
+    }
+}
